1ca2f1fbde873a4149de600714b934f683cc6195
sadovnikov <xomjachok13@gmail.com>
2023-04-15 09:10:42 +0300

    init

---
+++ frontend/src/components/View.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavigationView />
+    111
+  </div>
+</template>
+
+<script>
+  import NavigationView from './NavigationView.vue'
+  export default {
+    name: 'MainView',
+    components: {NavigationView},
+  }
+</script>
---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: 'src',
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+})
