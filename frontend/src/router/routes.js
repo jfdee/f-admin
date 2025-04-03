7d032c06e1fbd69b2f5be71b6f257ba3edf1e3c8
Anatoly <xomjachok13@gmail.com>
2025-04-03 20:04:49 +0300

    редирект / -> /admin

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,7 +2,7 @@ import adminRoutes from '@/components/routes'
 export default [
   {
     path: '/',
-    redirect: r => ({name: 'components-admin-view', params: r.params}),
+    redirect: r => ({name: 'admin-components-page', params: r.params}),
     children: [...adminRoutes],
   },
 ]
