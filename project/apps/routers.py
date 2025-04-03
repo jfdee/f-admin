1ea09056257b5e7e1e179b0efd9dcc20c3541750
Anatoly <xomjachok13@gmail.com>
2025-04-03 20:00:38 +0300

    шаблон бекенда переехал в f-admin-template

--- project/apps/routers.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from f_admin_backend.routers import router as admin_router
-
-router = APIRouter(prefix='/api', tags=['api'])
-router.include_router(admin_router)
-
-
-__all__ = ('router',)
--- venv.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if not exist venv (
-    echo "init venv..."
-    CALL python -m venv venv
-)
-
-CALL venv\Scripts\activate
-CALL python -m pip install -r requirements.txt
